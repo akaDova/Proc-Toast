caafa23628686d2f5eac0e29e6043a1dfad19758
akaDova <miraak.dova@gmail.com>
2019-03-18 21:50:44 +0300

    wait for child process ending

--- ProcToast.cpp
+++ ProcToast.cpp
@@ -1,21 +1,50 @@
-﻿// ProcToast.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
+#include <Windows.h>
 
-int main()
+
+int main(int argc, char **args)
 {
-    std::cout << "Hello World!\n"; 
+	std::string cmdLine = "";
+	for (int i = 1; i < argc; i++) {
+		std::cout << args[i] << std::endl;
+		cmdLine += args[i];
+		cmdLine += " ";
+	}
+	STARTUPINFOA si;
+	PROCESS_INFORMATION pi;
+	LPSTR cmdLine_p = const_cast<char *>(cmdLine.c_str());
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+	printf_s("cmdLine_p: %s\n", cmdLine_p);
+	if (!CreateProcessA(NULL,   // No module name (use command line)
+		cmdLine_p,        // Command line
+		NULL,           // Process handle not inheritable
+		NULL,           // Thread handle not inheritable
+		FALSE,          // Set handle inheritance to FALSE
+		0,              // No creation flags
+		NULL,           // Use parent's environment block
+		NULL,           // Use parent's starting directory 
+		&si,            // Pointer to STARTUPINFO structure
+		&pi)           // Pointer to PROCESS_INFORMATION structure
+		)
+	{
+		printf_s("CreateProcess failed (%d).\n", GetLastError());
+		return 1;
+	}
+
+	// Wait until child process exits.
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	// Close process and thread handles. 
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	MessageBoxA(NULL, cmdLine.c_str(), ("command line"), MB_OK);
+
+	
+
+	return 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
